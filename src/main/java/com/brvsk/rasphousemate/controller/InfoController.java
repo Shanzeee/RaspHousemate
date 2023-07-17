c91d006e0750748bae31c9661caddcd1e9a15e4d
Kacper <kacpersjuszb@gmail.com>
2023-07-17 18:17:21 +0200

    Info feature

---
+++ src/main/java/com/brvsk/rasphousemate/controller/InfoController.java
@@ -0,0 +1,63 @@
+package com.brvsk.rasphousemate.controller;
+
+import com.brvsk.rasphousemate.service.InfoService;
+
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/info")
+@RequiredArgsConstructor
+public class InfoController {
+
+    private final InfoService infoService;
+
+
+    @GetMapping(path = "/platform", produces = "application/json")
+    public Map<String, String> getPlatformInfo() {
+        return infoService.getPlatformInfo();
+    }
+
+    @GetMapping(path = "/hardware", produces = "application/json")
+    public Map<String, String> getHardwareInfo() {
+        return infoService.getHardwareInfo();
+    }
+
+    @GetMapping(path = "/memory", produces = "application/json")
+    public Map<String, Float> getMemoryInfo() {
+        return infoService.getMemoryInfo();
+    }
+
+
+    @GetMapping(path = "/os", produces = "application/json")
+    public Map<String, String> getOsInfo() {
+        return infoService.getOsInfo();
+    }
+
+    @GetMapping(path = "/java", produces = "application/json")
+    public Map<String, String> getJavaInfo() {
+        return infoService.getJavaInfo();
+    }
+
+
+    @GetMapping(path = "/network", produces = "application/json")
+    public Map<String, String> getSystemInfo() {
+        return infoService.getSystemInfo();
+    }
+
+
+    @GetMapping(path = "/codec", produces = "application/json")
+    public Map<String, Boolean> getCodecInfo() {
+        return infoService.getCodecInfo();
+    }
+
+    @GetMapping(path = "frequencies", produces = "application/json")
+    public Map<String, Long> getClockInfo() {
+        return infoService.getClockInfo();
+    }
+}
